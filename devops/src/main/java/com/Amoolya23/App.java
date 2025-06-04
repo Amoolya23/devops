c071ddfbfcd25071abaac831ef21f508d87ff0c7
Moulya <moulya203@gmail.com>
2025-06-04 20:58:53 +0530

    Initial commit

---
+++ devops/src/main/java/com/Amoolya23/App.java
@@ -0,0 +1,13 @@
+package com.Amoolya23;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
